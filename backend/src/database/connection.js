16a48468fc595eb419653d05bf7f0e692757e9df
Hiragy7 <hiragi.barth@gmail.com>
2020-04-02 21:51:38 -0300

    Foi a apresentação para testes automatizados com a 'supertest' e segurança nas rotas, com o 'celebrate'

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,8 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-const connecton = knex(configuration.development);
+const config = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development;
+
+const connecton = knex(config);
 
 module.exports = connecton;
